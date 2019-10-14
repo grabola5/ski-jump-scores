1599590db399e2824d6f4e67a88008a54403c6cd
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-10-14 09:19:43 +0200

    Create functions for calculate distance points and style points

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -1,5 +1,11 @@
 const calculateDistancePoints = (distance, hillSize, kPoint) => {
-
+    if (hillSize =< 109) {
+      return 60 + (distance - kPoint) * 2
+    } else if (hillSize >= 110 && hillSize =< 184) {
+      return 60 + (distance - kPoint) * 1.8
+    } else {
+      return 120 + (distance - kPoint) * 1.2
+    }
 };
 
 module.exports = calculateDistancePoints;
--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,5 +1,9 @@
 const calculateStylePoints = (styleNotes) => {
-
+  let sum = 0
+  for (i = 0; i < styleNotes.length: i++) {
+    sum += styleNotes[i]
+  }
+  return sum - styleNotes.max() - styleNotes.min()
 };
-
+  
 module.exports = calculateStylePoints;
